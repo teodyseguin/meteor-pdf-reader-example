b14b566ef02dc7aae36b6010c1681b28d4d64ddb
Teody C. Seguin <teodyseguin@gmail.com>
2016-10-06 17:03:18 +0800

    refactor listFiles()
    - Made the said function as an asynchronous function by adding a
    callback argument, which is then returned containing the files
    result

--- server/main.js
+++ server/main.js
@@ -2,7 +2,13 @@ import { Meteor } from 'meteor/meteor';
 
 // npm packages
 import shelljs from 'shelljs/global';
+import pdfjsDist from 'pdfjs-dist';
+import fs from 'fs';
 
+/**
+ * On startup of the server, we need to create
+ * 2 basic folders: .uploads/ and tmp/
+ */
 Meteor.startup(() => {
 	UploadServer.init({
 		tmpDir: '/.uploads/tmp',
@@ -10,50 +16,73 @@ Meteor.startup(() => {
 		checkCreateDirectories: true
 	});
 
+	// we also create a complete/ folder in which
+	// we intend to copy all pdf's that are complete
+	// scanned already
 	createCompleteFolder();
 });
 
+/**
+ * List of methods that can be called from the client side
+ * The soul purpose of each of these methods are to provide
+ * the list of pdf filenames and their total number, depending
+ * on which folder they are currently in
+ */
 Meteor.methods({
 	'pdfList'() {
-		let all = listFiles('/.uploads/*.pdf');
-
-		return {
-			files: all,
-			count: all.length
-		};
+		return listFiles('/.uploads/*.pdf', true, (result) => {
+			return {
+				files: result,
+				count: result.length
+			};
+		});
 	},
 	'pdfComplete'() {
-		let all = listFiles('/.uploads/complete/*.pdf');
-
-		return {
-			files: all,
-			count: all.length
-		};
+		return listFiles('/.uploads/complete/*.pdf', true, (result) => {
+			return {
+				files: result,
+				count: result.length
+			};
+		});
 	},
 	'pdfInProgress'() {
-		let all = listFiles('/.uploads/inprogress/*.pdf');
-
-		return {
-			files: all,
-			count: all.length
-		};
+		return listFiles('/.uploads/inprogress/*.pdf', true, (result) => {
+			return {
+				files: result,
+				count: result.length
+			};
+		});
 	}
 });
 
-function listFiles(location) {
-	let files = [];
+function listFiles(location, pdfOnly, callback) {
+	let files = [],
+		counter = 0;
 
-	ls(location).forEach(file => {
-		files.push(file);
+	ls(location).forEach((file, index, array) => {
+		let obj = {};
+		
+		if (pdfOnly) {
+			let data = new Uint8Array(fs.readFileSync(file));
+		
+			PDFJS.getDocument(data)
+				.then(pdfDocument => {
+					obj.count = pdfDocument.numPages;
+				});
+		}
+
+		obj.name = file;
+		obj.id = counter++;
+		files.push(obj);
 	});
 
-	return files;
+	return callback(files);
 }
 
 function createCompleteFolder() {
-	let files = listFiles('/.uploads');
-
-	if (!files['complete']) {
-		mkdir('/.uploads/complete');
-	}
+	listFiles('/.uploads', false, (result) => {
+		if (!result['complete']) {
+			mkdir('/.uploads/complete');
+		}
+	});
 }
