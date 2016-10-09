d186a14588a7ef25dd55cc1bfe6e3520c9dd9215
Teody C. Seguin <teodyseguin@gmail.com>
2016-10-09 10:35:14 +0800

    use db service
    - Import the DbService and use it to store the data to mongo

--- client/views/pdfs.id.js
+++ client/views/pdfs.id.js
@@ -1,23 +1,29 @@
-Template.pdfsId.events({
-	'submit .response'() {
-		Pdfs.insert({
-			responseId: 1,
-			admissionServices: '',
-			friendlinessOfDoctors: '',
-			friendlinessOfNurses: '',
-			friendlinessOfAlliedHealth: '',
-			cleanlinessOfAccommodation: '',
-			qualityOfFoodDrinks: '',
-			dischargeServices: '',
-			firstName: '',
-			surName: '',
-			iStayInWard: 1,
-			compliment: '',
-			complaint: '',
-			provideMoreInfo: false,
-			attendPatientFocusGroup: false
-		});
+import { DbService } from '../../imports/services/db.js';
 
-		return false;
+Template.pdfsId.events({
+	'click #save-response'(event) {
+		let pdf = new Pdf();
+
+		pdf.responseId = 1;
+		pdf.admissionServices = event.target.form.admission.value;
+		pdf.friendlinessOfDoctors = event.target.form.friendlinessd.value;
+		pdf.friendlinessOfNurses = event.target.form.friendlinessn.value;
+		pdf.friendlinessOfAlliedHealth = event.target.form.friendlinessa.value;
+		pdf.cleanlinessOfAccommodation = event.target.form.cleanliness.value;
+		pdf.qualityOfFoodDrinks = event.target.form.quality.value;
+		pdf.dischargeServices = event.target.form.discharge.value;
+		pdf.firstName = event.target.form.firstname.value;
+		pdf.surName = event.target.form.surname.value;
+		pdf.iStayInWard = parseInt(event.target.form.stay.value);
+		pdf.compliment = event.target.form.compliment.value;
+		pdf.complaint = event.target.form.complaint.value;
+		pdf.provideMoreInfo = event.target.form.providemoreinfo.checked;
+		pdf.attendPatientFocusGroup = event.target.form.attendpatientfocusgroup.checked;
+
+		DbService.save(pdf, function saveResponse(err) {
+			if (err) {
+				alert('Something weng wrong, please try again');
+			}
+		});
 	}
 });
