7a52d07541487ae71edaa119f7f85a92ca28cb4a
Teody C. Seguin <teodyseguin@gmail.com>
2016-10-10 13:49:10 +0800

    add uploads model schema

---
+++ models/uploads.js
@@ -0,0 +1,21 @@
+import { Class } from 'meteor/jagi:astronomy';
+import { Mongo } from 'meteor/mongo';
+
+Uploads = new Mongo.Collection('uploads');
+Uploads.publicFields = {
+	_id: 1,
+	filename: 1,
+	status: 1
+};
+Upload = Class.create({
+	name: 'Upload',
+	collection: Uploads,
+	fields: {
+		filename: String,
+		status: {
+			type: String,
+			default: 'uploaded'
+		}
+	},
+	secured: false
+});
