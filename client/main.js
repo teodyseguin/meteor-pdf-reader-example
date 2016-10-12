98a4b667dac883aa3b5833083aee2c55c76fe9c4
Teody C. Seguin <teodyseguin@gmail.com>
2016-10-12 23:25:35 +0800

    refactor client main js
    - import page uid js's
    - use Upload object, instead of Uploads
    - rename pdfUploaded helper function to pdfPending

--- client/main.js
+++ client/main.js
@@ -1,25 +1,34 @@
+// meteor packages
 import { Template } from 'meteor/templating';
-
+// npm packages
 import _ from 'lodash';
-
+// templates
 import './main.html';
 
+import '../imports/ui/pages/pdfs.pending.js';
+import '../imports/ui/pages/pdfs.inprogress.js';
+import '../imports/ui/pages/pdfs.complete.js';
+
 Template.mainContent.onCreated(function() {
-	Meteor.subscribe('list.pdfs', ['uploaded', 'inprogress', 'complete']);
+	Meteor.subscribe('list.pdfs', [
+		'pending', 
+		'inprogress', 
+		'complete'
+	]);
 });
 
 Template.mainContent.helpers({
 	pdfComplete: function() {
-		let result = Uploads.find().fetch();
+		let result = Upload.find().fetch();
 		return _.filter(result, ['status', 'complete']).length;
 	},
 	pdfInProgress: function() {
-		let result = Uploads.find().fetch();
+		let result = Upload.find().fetch();
 		return _.filter(result, ['status', 'inprogress']).length;
 	},
-	pdfUploaded: function() {
-		let result = Uploads.find().fetch();
-		return _.filter(result, ['status', 'uploaded']).length;
+	pdfPending: function() {
+		let result = Upload.find().fetch();
+		return _.filter(result, ['status', 'pending']).length;
 	}
 });
 
